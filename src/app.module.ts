f83539b186d0c077b760bd7ff86e10f7d474892b
Mateo Loaiza <loaiza.mateo.7291@eam.edu.co>
2024-10-06 22:02:53 -0500

    Add auto redirect and remove default from swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DiariosModule } from './diarios/diarios.module';
 import { NotificacionModule } from './notificacion/notificacion.module';
 
 @Module({
   imports: [DiariosModule, NotificacionModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
